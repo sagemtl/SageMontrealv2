3beb32f443e4b6e781da6ca1d4f58711a8a6cc17
Yu Tong Liu <yutongliuytl@gmail.com>
2020-05-08 19:58:11 -0400

    Creating Gatsby starter template

---
+++ src/pages/index.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+import CheckoutForm from "../components/checkout";
+
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe('pk_test_JJ1eMdKN0Hp4UFJ6kWXWO4ix00jtXzq5XG');
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
