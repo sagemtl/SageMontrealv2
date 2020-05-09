5e2e0eee2a04e45ef764c63b496ef936d8c49a64
Xinrui <xinruili@192.168.0.120>
2020-05-08 21:11:41 -0400

    Added dsstore to gitignore

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,6 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-
       <Elements stripe={stripePromise}>
         <CheckoutForm />
       </Elements>
