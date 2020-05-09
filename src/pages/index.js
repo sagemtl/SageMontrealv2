671f013f3c1066a09a1e1c27542a7ca56fcd6d0c
Tony <36376980+Tony9984@users.noreply.github.com>
2020-05-08 21:04:07 -0400

    Setup ESLint & fix initial errors (#1)
    Co-authored-by: Tony Ou <tony.ou@ibm.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import CheckoutForm from "../components/checkout";
-
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import CheckoutForm from '../components/checkout';
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
@@ -20,13 +18,12 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-
       <Elements stripe={stripePromise}>
         <CheckoutForm />
       </Elements>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
