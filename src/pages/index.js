2265744c57de121059d218cf887dc75546e4dcac
Yu Tong Liu <yutongliuytl@gmail.com>
2020-05-11 20:05:38 -0400

    Adding MongoDB plugin connection

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
@@ -9,21 +9,48 @@ import CheckoutForm from '../components/checkout';
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_JJ1eMdKN0Hp4UFJ6kWXWO4ix00jtXzq5XG');
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC);
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Elements stripe={stripePromise}>
-        <CheckoutForm />
-      </Elements>
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-);
+const IndexPage = (props) => {
+  const moods = props.data.allMongodbHeroku8Pxd36BkMoodboards.edges;
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      </div>
+      <div>
+        {moods.map((mood) => (
+          <div key={mood.node.id}>
+            <img width="300px" src={mood.node.imagePath} alt="Sage mood" />
+            <h2>{mood.node.name}</h2>
+            <p>{mood.node.instagram}</p>
+          </div>
+        ))}
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  );
+};
 
 export default IndexPage;
+
+export const pageQuery = graphql`
+  query {
+    allMongodbHeroku8Pxd36BkMoodboards {
+      edges {
+        node {
+          id
+          name
+          instagram
+          imagePath
+        }
+      }
+    }
+  }
+`;
