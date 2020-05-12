edc4aefe48d62bd4b5490450a61026198b963727
Cindy Chen <cxy2015ca@gmail.com>
2020-05-11 22:51:49 -0400

    added stripe plug in

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,8 +46,13 @@ module.exports = {
         },
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-source-stripe',
+      options: {
+        objects: ['Sku'],
+        secretKey: process.env.STRIPE_SECRET,
+        // downloadFiles: true,
+      }
+    },
   ],
 };
