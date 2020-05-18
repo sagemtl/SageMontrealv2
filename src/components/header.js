97a10308cffbff68919f547703b7c2f18aeb4925
Yu Tong Liu <yutongliuytl@gmail.com>
2020-05-17 22:30:13 -0400

    Adding navbar box (#4)

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,109 @@
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
+import Img from 'gatsby-image';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+const styles = {
+  navbox: {
+    position: 'fixed',
+    left: 30,
+    top: 30,
+    width: 175,
+    height: 225,
+    border: '2px solid black',
+    borderRadius: 15,
+    backgroundColor: 'white',
+    zIndex: 10,
+    overflow: 'hidden',
+  },
+  logoImage: {
+    width: 70,
+    height: 'auto',
+    margin: '0 auto',
+    top: 25,
+  },
+  link: {
+    height: 50,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    color: 'black',
+    textDecoration: 'none',
+  },
+  linkSelected: {
+    height: 50,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    color: 'white',
+    textDecoration: 'none',
+    backgroundColor: '#154734',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 0,
+    marginTop: 0,
+  },
+};
+
+const Header = ({ siteTitle }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "LOGO.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <div style={styles.navbox}>
+      <header>
+        <div>
+          <Link to="/">
+            <Img
+              style={styles.logoImage}
+              fluid={data.placeholderImage.childImageSharp.fluid}
+              alt={siteTitle}
+            />
+          </Link>
+        </div>
+      </header>
+      <hr
+        style={{
+          marginTop: 45,
+          marginBottom: 0,
+          height: 2,
+          backgroundColor: 'black',
+        }}
+      />
+      <div>
+        <Link style={styles.link} to="/shop">
+          <h2 style={styles.text}>Boutique</h2>
         </Link>
-      </h1>
+      </div>
+      <hr
+        style={{
+          marginTop: 0,
+          marginBottom: 0,
+          height: 2,
+          backgroundColor: 'black',
+        }}
+      />
+      <div>
+        <Link style={styles.link} to="/shop">
+          <h2 style={styles.text}>Lookbook</h2>
+        </Link>
+      </div>
     </div>
-  </header>
-);
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
