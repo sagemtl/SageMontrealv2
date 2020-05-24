d507ef4926266b6de155b3a0bf286a9f03029cfa
Tony <tonyou1999@hotmail.com>
2020-05-23 20:14:38 -0400

    Add styling for product page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
+import './styles/layout.scss';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,18 +19,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          alignItems: 'center',
-          margin: '0 auto',
-          width: '100%',
-        }}
-      >
-        {children}
-      </div>
+      <div className="layout">{children}</div>
     </>
   );
 };
