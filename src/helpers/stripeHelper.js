cb800fdea6aec1911914783eede0470688f4e959
Cindy Chen <cxy2015ca@gmail.com>
2020-05-24 22:43:12 -0400

    cleaned code

--- src/helpers/stripeHelper.js
+++ src/helpers/stripeHelper.js
@@ -1,27 +1,4 @@
 
-// export function updateProduct(prod_id, productContent) {
-//     // var product_info = 
-
-//     stripe.products.update(
-//         prod_id,
-//         {description:"newwwwww description laaaa"},
-//         function(err, product) {
-//         // asynchronously called
-//             console.log("error: " + err);
-//             console.log("returned product: " + product);
-//             return product;
-//         }
-//     )
-//     // .then(
-//     //     function(result){
-//     //         console.log("returned product: " + product);
-//     //     },
-//     //     function(err){
-//     //         console.log("error: " + err);
-//     //     }
-//     // )
-// }
-
 
 export const getProductInfo = async (prod_id) => {
     var myHeaders = new Headers();
@@ -46,8 +23,3 @@ export const updateProduct = async (product) =>{
     
 }
 
-export const generateSlug = (name) => {
-    //global replacement of space
-    var slug = name.replace(/ /g, "-");
-    return slug;
-}
