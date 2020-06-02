5c2891bc2c62dc661791dd643633c850c57f1d71
Tony <tonyou1999@hotmail.com>
2020-06-02 01:53:45 -0400

    Change footer layout

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,13 +6,15 @@ import './styles/footer.scss';
 const Footer = () => {
   return (
     <div className="footer">
+      <div className="footer-icons">
+        <a href="https://addmeonqq.tumblr.com/post/184841955797/amp">
+          <img src={InstagramIcon} alt="Instagram" className="footer__icon" />
+        </a>
+        <a href="https://www.instagram.com/sagemtl/">
+          <img src={TumblrIcon} alt="Tumblr" className="footer__icon" />
+        </a>
+      </div>
       <h3 className="footer__header">© Sage Montreal 2020</h3>
-      <a href="https://addmeonqq.tumblr.com/post/184841955797/amp">
-        <img src={InstagramIcon} alt="Instagram" className="footer__icon" />
-      </a>
-      <a href="https://www.instagram.com/sagemtl/">
-        <img src={TumblrIcon} alt="Tumblr" className="footer__icon" />
-      </a>
     </div>
   );
 };
