63eddd4605fdd0a8fe0fdb4160ad02e113ef5324
Cindy Chen <cxy2015ca@gmail.com>
2020-06-03 21:13:18 -0400

    changed site icon to sage, fixed link for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,10 +7,10 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-icons">
-        <a href="https://addmeonqq.tumblr.com/post/184841955797/amp">
+        <a href="https://www.instagram.com/sagemtl/" target="_blank">
           <img src={InstagramIcon} alt="Instagram" className="footer__icon" />
         </a>
-        <a href="https://www.instagram.com/sagemtl/">
+        <a href="https://addmeonqq.tumblr.com/post/184841955797/amp" target="_blank">
           <img src={TumblrIcon} alt="Tumblr" className="footer__icon" />
         </a>
       </div>
