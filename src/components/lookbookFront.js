3b2ac400804d226cfdc7a7853aae7625f53601bc
Tony <tonyou1999@hotmail.com>
2020-06-13 03:10:23 -0400

    Finish adding lookbook images, still need to polish styling

--- src/components/lookbookFront.js
+++ src/components/lookbookFront.js
@@ -1,24 +1,59 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import Img from 'gatsby-image';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
 import './styles/lookbookFront.scss';
+import Fade from '@material-ui/core/Fade';
 
-const LookbookFront = ({ image, link, isMobile }) => {
-  return (
-    <Link to={link}>
-      <div className={isMobile ? 'lookbook-front-mobile' : 'lookbook-front'}>
-        {/*         
-        <Img className="" fluid={image} alt="Sage Lookbook" />
-      */}
+const LookbookFront = ({ label, images, isMobile }) => {
+  const [open, setOpen] = useState(false);
+
+  const modalContent = (
+    <Fade in={open}>
+      <div className="lookbook-modal-content">
+        {images.map((image, index) => {
+          return (
+            <img
+              src={image}
+              className="lookbook-front__image"
+              alt={`${label}-${index}`}
+            />
+          );
+        })}
       </div>
-    </Link>
+    </Fade>
+  );
+
+  return (
+    <>
+      <div
+        className={isMobile ? 'lookbook-front-mobile' : 'lookbook-front'}
+        onClick={() => setOpen(true)}
+      >
+        <h1 className="lookbook-front__header">{label}</h1>
+      </div>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        className="lookbook-modal"
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        {modalContent}
+      </Modal>
+    </>
   );
 };
 
 LookbookFront.propTypes = {
-  link: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default LookbookFront;
--- src/pages/lookbook.js
+++ src/pages/lookbook.js
@@ -9,6 +9,68 @@ import '../styles/lookbook.scss';
 const LookbookMenu = ({ uri }) => {
   const [width, setWidth] = useState(window.innerWidth);
 
+  const collections = [
+    {
+      label: 'Capsule 01: Swish',
+      images: [
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1583035301/lookbookSwish/swish_lookbook_u1laoy.jpg',
+      ],
+    },
+    {
+      label: 'Winter 20: Steam',
+      images: [
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1579583184/lookbookSteam/1_gjlmik.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1579583184/lookbookSteam/2_pk948p.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1579583184/lookbookSteam/3_g45sch.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1579583184/lookbookSteam/4_hphb4t.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1579583184/lookbookSteam/5_aptyse.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1579583184/lookbookSteam/6_hy2piq.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1579583185/lookbookSteam/7_n2tkl9.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1579583184/lookbookSteam/8_smacic.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1579583184/lookbookSteam/9_esocvv.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1579583184/lookbookSteam/10_iezm3p.jpg',
+      ],
+    },
+    {
+      label: 'Summer 19: Transit',
+      images: [
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337613/lookbook-desktop/1_i5socf.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337443/lookbook-desktop/2_eayxmf.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337443/lookbook-desktop/3_kl0zea.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337443/lookbook-desktop/4_vpaedq.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337444/lookbook-desktop/5_d9t2jm.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337444/lookbook-desktop/6_ijjucl.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337444/lookbook-desktop/7_udmk7x.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337445/lookbook-desktop/8_cc6pze.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337444/lookbook-desktop/9_h5owzi.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337445/lookbook-desktop/10_dps5iq.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337444/lookbook-desktop/11_a2zpzr.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337444/lookbook-desktop/12_vmjoxv.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337445/lookbook-desktop/13_mng7mw.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337445/lookbook-desktop/14_nqgraf.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337445/lookbook-desktop/15_zgarnk.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561337445/lookbook-desktop/16_iqv3bj.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1561338416/lookbook-desktop/17_kxmjkh.jpg',
+      ],
+    },
+    {
+      label: 'Winter 18: 20666',
+      images: [
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1580350739/lookbookFirst/DSC_0593_dqqatl.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1580350739/lookbookFirst/Untitled-2_jb5y2r.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1580350739/lookbookFirst/DSC_0811_waybex.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1580350739/lookbookFirst/DSC_0791_decats.jpg',
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1580350739/lookbookFirst/DSC_0820_bwi8bi.jpg',
+      ],
+    },
+    {
+      label: 'Spring 18: 仙人集团',
+      images: [
+        'https://res.cloudinary.com/sage-montreal/image/upload/v1556419772/lookbook_mvygkt.jpg',
+      ],
+    },
+  ];
+
   useEffect(() => {
     window.addEventListener('resize', () => setWidth(window.innerWidth));
   }, []);
@@ -19,9 +81,13 @@ const LookbookMenu = ({ uri }) => {
     <Layout current={uri}>
       <div className={isMobile ? 'lookbook-mobile' : 'lookbook'}>
         <div className="lookbook-scroll">
-          {[0, 0, 0, 0, 0].map(() => {
+          {collections.map(({ label, images }) => {
             return (
-              <LookbookFront link="/shop" image="test" isMobile={isMobile} />
+              <LookbookFront
+                images={images}
+                label={label}
+                isMobile={isMobile}
+              />
             );
           })}
         </div>
