5bd9869879dda569b4d78bfa70aa90d7f9692b22
Tony <tonyou1999@hotmail.com>
2020-06-15 22:41:17 -0400

    Add CMS styling

--- src/pages/cms.js
+++ src/pages/cms.js
@@ -1,25 +1,35 @@
 import React, { useState } from 'react';
-import { Checkbox, Form, TextArea, Select } from 'semantic-ui-react';
 import { getProductInfo } from '../helpers/stripeHelper';
-import Layout from '../components/layout';
+
+import '../styles/cms.scss';
 
 const CMS = () => {
-  const [prod_id, setProd_id] = useState('');
+  // Product fields
+
+  const [productId, setProductId] = useState('');
   const [active, setActive] = useState(false);
   const [desc, setDesc] = useState('');
-  const [images, setImages] = useState([]);
+  // for the separated images url thingy, i was thinking we
+  // could just take the whole string and break it into
+  // separate urls once we send the request using split(',')
+  const [images, setImages] = useState('');
   const [shippable, setShippable] = useState(false);
   const [type, setType] = useState('');
+
+  // Lookbook fields
+
+  const [title, setTitle] = useState('');
+  const [coverUrl, setCoverUrl] = useState('');
+  const [lookbookImages, setLookbookImages] = useState('');
 
   // allow editing of product info
   const [edit, setEdit] = useState(false);
 
   const handleGetProduct = async (e) => {
     e.preventDefault();
-    var prod = await getProductInfo(prod_id);
+    var prod = await getProductInfo(productId);
     var prod = JSON.parse(prod);
     console.log(prod);
-    // setProd_id(prod.id);
     setActive(prod.active);
     setDesc(prod.description);
     setImages(prod.images);
@@ -28,49 +38,112 @@ const CMS = () => {
   };
 
   return (
-    <div>
-      <Form onSubmit={() => handleGetProduct()}>
-        <Form.Input
-          label="Product ID"
-          placeholder="Product ID"
-          onChange={(e) => setProd_id(e.target.value)}
-          value={prod_id || ''}
-        />
+    <div className="cms">
+      <div className="cms-product">
+        <h1>Product</h1>
+        <div className="cms__field">
+          <p className="cms__label">Product ID</p>
+          <input
+            placeholder="Product ID"
+            onChange={(e) => setProductId(e.target.value)}
+            value={productId}
+            className="cms__input"
+          />
+          <button
+            onClick={() => handleGetProduct()}
+            type="button"
+            className="cms__button"
+          >
+            Get Product
+          </button>
+        </div>
 
-        <button type="submit">Get Product</button>
-      </Form>
+        <div className="cms__field">
+          <p className="cms__label">Description</p>
+          <div>
+            <textarea
+              placeholder="Description"
+              onChange={(e) => setDesc(e.target.value)}
+              value={desc}
+              rows={4}
+              className="cms__input-area"
+            />
+          </div>
+        </div>
 
-      <Form >
-        <Form.Group>
-        <Form.Field
-          label="Description"
-          placeholder="Description"
-          onChange={(e) => setDesc(e.target.value)}
-          value={desc || ''}
-        />
+        <div className="cms__field">
+          <p className="cms__label">True/False?</p>
+          <div>
+            <input
+              placeholder="true/false"
+              onChange={(e) => {
+                setActive(e.target.value === 'true');
+              }}
+              value={active}
+              className="cms__input"
+            />
+          </div>
+        </div>
 
-        <Form.Field
-          label="Active"
-          placeholder="true/false"
-          onChange={(e) => {
-            e.target.value == "true"? setActive(true) : setActive(false)
-          }}
-          value={active || ''}
-        />
+        <div className="cms__field">
+          <p className="cms__label">Images URL</p>
+          <div>
+            <input
+              placeholder="Comma seperated URLs"
+              onChange={(e) => setImages(e.target.value)}
+              value={images}
+              className="cms__input"
+            />
+          </div>
+        </div>
 
-        <Form.Field
-          control={TextArea}
-          label="Images URL"
-          placeholder="comma seperated URLs"
-          onChange={(e) => setImages(e.target.value)}
-          value={images.toString() || ''}
-        />
-        
-        {/* {images? (images.map((url)=>{
-            <img src={url} alt="product images"/>
-          })) : null} */}
-          </Form.Group>
-        </Form>
+        <button type="button" className="cms__button">
+          Add Product
+        </button>
+      </div>
+
+      <div className="cms-lookbook">
+        <h1>Lookbook</h1>
+        <div className="cms__field">
+          <p className="cms__label">Lookbook title</p>
+          <div>
+            <input
+              placeholder="Product ID"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              className="cms__input"
+            />
+          </div>
+        </div>
+
+        <div className="cms__field">
+          <p className="cms__label">Cover URL</p>
+          <div>
+            <input
+              placeholder="URL of cover picture"
+              onChange={(e) => setCoverUrl(e.target.value)}
+              value={coverUrl}
+              className="cms__input"
+            />
+          </div>
+        </div>
+
+        <div className="cms__field">
+          <p className="cms__label">Images URL</p>
+          <div>
+            <input
+              placeholder="Comma seperated URLs"
+              onChange={(e) => setLookbookImages(e.target.value)}
+              value={lookbookImages}
+              className="cms__input"
+            />
+          </div>
+        </div>
+
+        <button type="button" className="cms__button">
+          Add lookbook
+        </button>
+      </div>
     </div>
   );
 };
