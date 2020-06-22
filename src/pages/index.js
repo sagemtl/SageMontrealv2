ce4bb226e0f54dd7bbdd17026e7f7950eb0bcc7b
Tony <tonyou1999@hotmail.com>
2020-06-22 17:41:11 -0400

    Refactor code

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,27 +3,37 @@ import PropTypes from 'prop-types';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import '../styles/index.scss';
-import Background1 from '../images/sage-background.jpg';
-import Background2 from '../images/sage-background-2.jpg';
+import classNames from 'classnames';
 
 const IndexPage = ({ uri }) => {
   const [swap, setSwap] = useState(true);
 
+  const indexClass = classNames({
+    index: swap,
+    'index-flip': !swap,
+  });
+
   return (
     <Layout current={uri} footerTransparent hideCart>
       <SEO title="Home" />
-      <div
-        className="index"
-        style={
-          swap
-            ? { backgroundImage: `url(${Background1})` }
-            : { backgroundImage: `url(${Background2})` }
-        }
-      >
-        <label className="switch">
+      <div className={indexClass}>
+        <label className="index-toggle">
           <input type="checkbox" />
-          <span onClick={() => setSwap(!swap)} className="slider round" />
+          <span
+            onClick={() => setSwap(!swap)}
+            className="index-toggle__slider"
+          />
         </label>
+        <div className="index-text">
+          <h1 className="index-text__header--main">
+            <b>Sage Montreal</b>
+          </h1>
+          <h2 className="index-text__header--1">We'll be back soon!</h2>
+          <h2 className="index-text__header--2">66666</h2>
+          <h2 className="index-text__header--3">66666</h2>
+          <h2 className="index-text__header--4">仙仙仙</h2>
+          <h2 className="index-text__header--5">仙仙仙</h2>
+        </div>
       </div>
     </Layout>
   );
