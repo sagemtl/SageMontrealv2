766d328277b29e1423871913eb7a2f4e276f941e
Tony <36376980+Tony9984@users.noreply.github.com>
2020-07-08 21:33:23 -0400

    Fix/styling (#23)
    * modify styles for footer
    
    * Move play button & remove mobile box
    
    * Adjust shop images size
    
    * Adjust lookbook animations
    
    Co-authored-by: Tony Ou <tony.ou@ibm.com>

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,11 +3,10 @@ import PropTypes from 'prop-types';
 import { useLocation } from '@reach/router';
 import PauseIcon from '@material-ui/icons/Pause';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import InstagramIcon from '../images/instagram.svg';
-import TumblrIcon from '../images/tumblr.svg';
+import InstagramIcon from '@material-ui/icons/Instagram';
 import './styles/footer.scss';
-import { GlobalContext } from '../context/Provider';
 import classNames from 'classnames';
+import { GlobalContext } from '../context/Provider';
 
 const Footer = ({ transparent, color }) => {
   const { state, dispatch } = useContext(GlobalContext);
@@ -30,22 +29,13 @@ const Footer = ({ transparent, color }) => {
 
   return (
     <div className={transparent ? 'footer-transparent' : 'footer'}>
-      <div className="footer-icons">
-        <a
-          href="https://www.instagram.com/sagemtl/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={InstagramIcon} alt="Instagram" className={iconClass} />
-        </a>
-        <a
-          href="https://addmeonqq.tumblr.com/post/184841955797/amp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={TumblrIcon} alt="Tumblr" className={iconClass} />
-        </a>
-      </div>
+      <a
+        href="https://www.instagram.com/sagemtl/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <InstagramIcon className={iconClass} />
+      </a>
       {pathname === '/shop' && (
         <button
           type="button"
@@ -59,9 +49,7 @@ const Footer = ({ transparent, color }) => {
           )}
         </button>
       )}
-      <h3 className="footer__header" style={{ color }}>
-        © Sage Montreal 2020
-      </h3>
+      <h3 className="footer__header">© Sage Montreal 2020</h3>
     </div>
   );
 };
