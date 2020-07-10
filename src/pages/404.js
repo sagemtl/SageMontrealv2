edcd0cdb1685fd8c0f55905cee040a8a96497634
Tony <tonyou1999@hotmail.com>
2020-07-10 13:26:12 -0400

    feat: add contact us page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,8 @@ import Layout from '../components/layout';
 
 import '../styles/404.scss';
 
-const NotFoundPage = () => (
-  <Layout>
+const NotFoundPage = ({ uri }) => (
+  <Layout current={uri} hideCart>
     <div className="not-found">
       <h1 className="not-found__header">404 | NOT FOUND</h1>
       <p className="not-found__text">
