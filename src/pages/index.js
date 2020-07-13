e8c0ddd17d99956c6eb33971eaee973a14ef9e39
Tony Ou <tony.ou@ibm.com>
2020-07-13 12:52:09 -0400

    feat: add artstep iframe placeholder

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,14 +32,16 @@ const IndexPage = ({ uri }) => {
             />
           </label>
           <div className="index-text">
+            <iframe
+              width="860"
+              height="615"
+              src="https://www.artsteps.com/embed/5f035eac83ecaf0b05dfd83c/560/315"
+              frameBorder="0"
+              allowFullScreen
+            />
             <h1 className="index-text__header--main">
               <b>Sage Montreal</b>
             </h1>
-            <h2 className="index-text__header--1">We'll be back soon!</h2>
-            <h2 className="index-text__header--2">66666</h2>
-            <h2 className="index-text__header--3">66666</h2>
-            <h2 className="index-text__header--4">仙仙仙</h2>
-            <h2 className="index-text__header--5">仙仙仙</h2>
           </div>
         </div>
       </>
