b3ea00b677b64de405f610a960a9c2e98a19dd89
Tony Ou <tony.ou@ibm.com>
2020-08-26 14:24:11 -0400

    fix: remove duplicate plugin in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-layout`,
     {
       resolve: `gatsby-plugin-layout`,
       options: {
