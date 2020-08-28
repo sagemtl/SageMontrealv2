23dc9ffd59a6db4e3d73e53b781a8a84e0716073
Tony <tonyou1999@hotmail.com>
2020-08-28 19:03:30 -0400

    fix: restore mobile navbar

--- src/components/Header/containers/Header.js
+++ src/components/Header/containers/Header.js
@@ -1,22 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import HeaderDesktop from '../components/HeaderDesktop';
 import HeaderMobile from '../components/HeaderMobile';
 import '../styles/header.scss';
 
-const Header = ({ current }) => {
-  const widthVal = typeof window !== `undefined` ? window.innerWidth : 0;
-  const [width, setWidth] = useState(widthVal);
-
-  useEffect(() => {
-    if (typeof window !== `undefined`) {
-      window.addEventListener('resize', () => setWidth(window.innerWidth));
-    }
-  }, []);
-
+const Header = () => {
   return (
     <>
-      <HeaderDesktop current={current} display={width >= 900} />
-      <HeaderMobile display={width >= 900} />
+      <HeaderDesktop />
+      <HeaderMobile />
     </>
   );
 };
