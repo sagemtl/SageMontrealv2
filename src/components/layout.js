e4ccd7a3df64ca8d672e7eb1847d13d76d9b02e8
Yu Tong Liu <yutongliuytl@gmail.com>
2020-08-28 22:59:31 -0400

    Fixed bug layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import Header from './Header';
+import Footer from './footer';
 import Cart from './cart';
 import './styles/layout.scss';
 
